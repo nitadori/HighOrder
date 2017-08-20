a6c89dad02660f120cb7d8f15fcb68fb4b95e807
nitadori <keigo@riken.jp>
2017-08-20 21:57:17 +0900

    16h

---
+++ hermite16.h
@@ -0,0 +1,268 @@
+#include "vector3.h"
+#include "taylor.h"
+
+struct Particle{
+	enum
+	{
+		NFORCE = 8,
+		ORDER  = 2*NFORCE,
+	};
+	unsigned long id;
+	dd_real dt;
+	dd_real tlast;
+	dd_real mass;
+
+	qvec3 coord[2 + ORDER]; // pos-vel, forces, interpolates
+};
+
+struct Predictor{
+	dd_real mass;
+	qvec3 coord[8];
+
+	void predict(const dd_real tnext, const Particle &p){
+		const dd_real dt = tnext - p.tlast;
+		mass = p.mass;
+		coord[0] = taylor<15>(dt, &p.coord[0]);
+		coord[1] = taylor<14>(dt, &p.coord[1]);
+		coord[2] = taylor<13>(dt, &p.coord[2]);
+		coord[3] = taylor<12>(dt, &p.coord[3]);
+		coord[4] = taylor<11>(dt, &p.coord[4]);
+		coord[5] = taylor<10>(dt, &p.coord[5]);
+		coord[6] = taylor< 9>(dt, &p.coord[6]);
+		coord[7] = taylor< 8>(dt, &p.coord[7]);
+	}
+	void zero_predict(const Particle &p){
+		mass     = p.mass;
+		coord[0] = p.coord[0];
+		coord[1] = p.coord[1];
+		coord[2] = p.coord[2];
+		coord[3] = p.coord[3];
+		coord[4] = p.coord[4];
+		coord[5] = p.coord[5];
+		coord[6] = p.coord[6];
+		coord[7] = p.coord[7];
+	}
+};
+
+struct Force{
+	qvec3 force[8]; // acc, jerk, snap, crackle, pop, pdot, pdot2, pdot3
+	void init_assign(Particle &p){
+		for(int m=0; m<Particle::NFORCE; m++){
+			p.coord[2+m] = force[m];
+		}
+	}
+};
+
+struct Corrector{
+	qvec3 pos;
+	qvec3 vel;
+	qvec3 force[8];
+	qvec3 fintp[8];
+
+	void correct(const Particle &p, const Force &f){
+		const dd_real h = 0.5 * p.dt;
+		const dd_real c0 = 0.5;
+		const dd_real c1 = c0 * h;
+		const dd_real c2 = c1 * (dd_real::div(1., 2.) * h);
+		const dd_real c3 = c2 * (dd_real::div(1., 3.) * h);
+		const dd_real c4 = c3 * (dd_real::div(1., 4.) * h);
+		const dd_real c5 = c4 * (dd_real::div(1., 5.) * h);
+		const dd_real c6 = c5 * (dd_real::div(1., 6.) * h);
+		const dd_real c7 = c6 * (dd_real::div(1., 7.) * h);
+
+		const dd_real d0 = c0;
+		const dd_real d1 = dd_real::div( 7.,   15.) * c1;
+		const dd_real d2 = dd_real::div(12.,   45.) * c2;
+		const dd_real d3 = dd_real::div( 2.,   13.) * c3;
+		const dd_real d4 = dd_real::div(16.,  195.) * c4;
+		const dd_real d5 = dd_real::div(16.,  429.) * c5;
+		const dd_real d6 = dd_real::div(64., 5005.) * c6;
+		const dd_real d7 = dd_real::div(16., 6435.) * c7;
+
+		const qvec3 *fr = f.force;
+		const qvec3 *fl = p.coord + 2;
+
+		// correct velocity
+		const qvec3 f0pl = d0 * (fr[0] + fl[0]);
+		const qvec3 f1mn = d1 * (fr[1] - fl[1]);
+		const qvec3 f2pl = d2 * (fr[2] + fl[2]);
+		const qvec3 f3mn = d3 * (fr[3] - fl[3]);
+		const qvec3 f4pl = d4 * (fr[4] + fl[4]);
+		const qvec3 f5mn = d5 * (fr[5] - fl[5]);
+		const qvec3 f6pl = d6 * (fr[6] + fl[6]);
+		const qvec3 f7mn = d7 * (fr[7] - fl[7]);
+
+		this->vel = p.coord[1] + p.dt * (f0pl - (f1mn - (f2pl - (f3mn - (f4pl - (f5mn - (f6pl - f7mn)))))));
+
+		// correct position
+		const qvec3 v0pl = d0 * (this->vel + p.coord[1]);
+		const qvec3 v1mn = d1 * (fr[0] - fl[0]);
+		const qvec3 v2pl = d2 * (fr[1] + fl[1]);
+		const qvec3 v3mn = d3 * (fr[2] - fl[2]);
+		const qvec3 v4pl = d4 * (fr[3] + fl[3]);
+		const qvec3 v5mn = d5 * (fr[4] - fl[4]);
+		const qvec3 v6pl = d6 * (fr[5] + fl[5]);
+		const qvec3 v7mn = d7 * (fr[6] - fl[6]);
+
+		this->pos = p.coord[0] + p.dt * (v0pl - (v1mn - (v2pl - (v3mn - (v4pl - (v5mn - (v6pl - v7mn)))))));
+
+		// copy forces
+		for(int i=0; i<8; i++) force[i] = f.force[i];
+	}
+
+	void interpolate(const Particle &p, const Force &f){
+		qvec3 fpl[8];
+		qvec3 fmn[8];
+		qvec3 fmid[16];
+		// qvec3 fleft [16];
+		qvec3 fright[16];
+
+		const dd_real h = 0.5 * p.dt;
+		const dd_real c0 = 0.5;
+		const dd_real c1 = c0 * h;
+		const dd_real c2 = c1 * (dd_real::div(1., 2.) * h);
+		const dd_real c3 = c2 * (dd_real::div(1., 3.) * h);
+		const dd_real c4 = c3 * (dd_real::div(1., 4.) * h);
+		const dd_real c5 = c4 * (dd_real::div(1., 5.) * h);
+		const dd_real c6 = c5 * (dd_real::div(1., 6.) * h);
+		const dd_real c7 = c6 * (dd_real::div(1., 7.) * h);
+
+		const qvec3 *fr = f.force;
+		const qvec3 *fl = p.coord + 2;
+
+		// f+, f-
+		{
+			fpl[0] = c0 * (fr[0] + fl[0]);
+			fmn[0] = c0 * (fr[0] - fl[0]);
+			fpl[1] = c1 * (fr[1] + fl[1]);
+			fmn[1] = c1 * (fr[1] - fl[1]);
+			fpl[2] = c2 * (fr[2] + fl[2]);
+			fmn[2] = c2 * (fr[2] - fl[2]);
+			fpl[3] = c3 * (fr[3] + fl[3]);
+			fmn[3] = c3 * (fr[3] - fl[3]);
+			fpl[4] = c4 * (fr[4] + fl[4]);
+			fmn[4] = c4 * (fr[4] - fl[4]);
+			fpl[5] = c5 * (fr[5] + fl[5]);
+			fmn[5] = c5 * (fr[5] - fl[5]);
+			fpl[6] = c6 * (fr[6] + fl[6]);
+			fmn[6] = c6 * (fr[6] - fl[6]);
+			fpl[7] = c7 * (fr[7] + fl[7]);
+			fmn[7] = c7 * (fr[7] - fl[7]);
+		}
+		// fmid
+		{
+			qvec3 evn[8], odd[8];
+
+			evn[1] = fmn[1] - 2.*fpl[2] + 3.*fmn[3] - 4.*fpl[4] +  5.*fmn[5] -  6.*fpl[6] +  7.*fmn[7];
+			evn[3] =                         fmn[3] - 4.*fpl[4] + 10.*fmn[5] - 20.*fpl[6] + 35.*fmn[7];
+			evn[5] =                                                  fmn[5] -  6.*fpl[6] + 21.*fmn[7];
+			evn[7] =                                                                            fmn[7];
+
+			odd[0] = fmn[0] - fpl[1] +    fmn[2] -    fpl[3] +    fmn[4] -     fpl[5] +     fmn[6] -     fpl[7];
+			odd[2] =                      fmn[2] - 3.*fpl[3] + 6.*fmn[4] - 10.*fpl[5] + 15.*fmn[6] - 21.*fpl[7];
+			odd[4] =                                              fmn[4] -  5.*fpl[5] + 15.*fmn[6] - 35.*fpl[7];
+			odd[6] =                                                                        fmn[6] -  7.*fpl[7];
+
+			fmid[ 8] = (1./2048.) * (-2145.*evn[1] + 693.*evn[3] - 525.*evn[5] + 1225.*evn[7]);
+			fmid[10] = (1./2048.) * ( 1001.*evn[1] - 297.*evn[3] + 189.*evn[5] -  245.*evn[7]);
+			fmid[12] = (1./2048.) * ( -273.*evn[1] +  77.*evn[3] -  45.*evn[5] +   49.*evn[7]);
+			fmid[14] = (1./2048.) * (   33.*evn[1] -   9.*evn[3] +   5.*evn[5] -    5.*evn[7]);
+
+			fmid[ 9] = (1./2048.) * ( 25025.*odd[0] - 2145.*odd[2] + 693.*odd[4] - 525.*odd[6]);
+			fmid[11] = (1./2048.) * (-12285.*odd[0] + 1001.*odd[2] - 297.*odd[4] + 189.*odd[6]);
+			fmid[13] = (1./2048.) * (  3465.*odd[0] -  273.*odd[2] +  77.*odd[4] -  45.*odd[6]);
+			fmid[15] = (1./2048.) * (  -429.*odd[0] +   33.*odd[2] -   9.*odd[4] +   5.*odd[6]);
+		}
+		fright[ 8] = fmid[8] + 9.*fmid[9] +  45.*fmid[10] + 165.*fmid[11] + 495.*fmid[12] + 1287.*fmid[13] + 3003.*fmid[14] + 6435.*fmid[15];
+		fright[ 9] =              fmid[9] +  10.*fmid[10] +  55.*fmid[11] + 220.*fmid[12] +  715.*fmid[13] + 2002.*fmid[14] + 5005.*fmid[15];
+		fright[10] =                             fmid[10] +  11.*fmid[11] +  66.*fmid[12] +  286.*fmid[13] + 1001.*fmid[14] + 3003.*fmid[15];
+		fright[11] =                                             fmid[11] +  12.*fmid[12] +   78.*fmid[13] +  364.*fmid[14] + 1365.*fmid[15];
+		fright[12] =                                                             fmid[12] +   13.*fmid[13] +   91.*fmid[14] +  455.*fmid[15];
+		fright[13] =                                                                              fmid[13] +   14.*fmid[14] +  105.*fmid[15];
+		fright[14] =                                                                                               fmid[14] +   15.*fmid[15];
+		fright[15] =                                                                                                                fmid[15];
+#if 0 // 12th-order interpolation for stability
+#endif
+		// rescale
+		{
+			const dd_real hi = 1.0 / h;
+			const dd_real s1 = hi;
+			const dd_real s2 = s1 * (2.*hi);
+			const dd_real s3 = s2 * (3.*hi);
+			const dd_real s4 = s3 * (4.*hi);
+			const dd_real s5 = s4 * (5.*hi);
+			const dd_real s6 = s5 * (6.*hi);
+			const dd_real s7 = s6 * (7.*hi);
+			const dd_real s8 = s7 * (8.*hi);
+			const dd_real s9 = s8 * (9.*hi);
+			const dd_real s10= s9 *(10.*hi);
+			const dd_real s11= s10*(11.*hi);
+			const dd_real s12= s11*(12.*hi);
+			const dd_real s13= s12*(13.*hi);
+			const dd_real s14= s13*(14.*hi);
+			const dd_real s15= s14*(15.*hi);
+
+			fintp[0] = s8 * fright[8];
+			fintp[1] = s9 * fright[9];
+			fintp[2] = s10* fright[10];
+			fintp[3] = s11* fright[11];
+			fintp[4] = s12* fright[12];
+			fintp[5] = s13* fright[13];
+			fintp[6] = s14* fright[14];
+			fintp[7] = s15* fright[15];
+		}
+	}
+	void commit(Particle &p){
+		p.coord[0] = pos;
+		p.coord[1] = vel;
+
+		p.coord[2] = force[0];
+		p.coord[3] = force[1];
+		p.coord[4] = force[2];
+		p.coord[5] = force[3];
+		p.coord[6] = force[4];
+		p.coord[7] = force[5];
+		p.coord[8] = force[6];
+		p.coord[9] = force[7];
+
+		p.coord[10] = fintp[0];
+		p.coord[11] = fintp[1];
+		p.coord[12] = fintp[2];
+		p.coord[13] = fintp[3];
+		p.coord[14] = fintp[4];
+		p.coord[15] = fintp[5];
+		p.coord[16] = fintp[6];
+		p.coord[17] = fintp[7];
+
+		p.tlast += p.dt;
+	}
+	void feedback(Predictor &pr){
+		pr.coord[0] = pos;
+		pr.coord[1] = vel;
+		pr.coord[2] = force[0]; // acc
+		pr.coord[3] = force[1]; // jrk
+		pr.coord[4] = force[2]; // snp
+		pr.coord[5] = force[3]; // crk
+		pr.coord[6] = force[4]; // pop
+		pr.coord[7] = force[5]; // pdot
+	}
+	void taylor_test(const Particle &p){
+		dvec3 df0 = p.coord[2] - taylor<15>(-p.dt, &force[0]);
+		dvec3 df1 = p.coord[3] - taylor<14>(-p.dt, &force[1]);
+		dvec3 df2 = p.coord[4] - taylor<13>(-p.dt, &force[2]);
+		dvec3 df3 = p.coord[5] - taylor<12>(-p.dt, &force[3]);
+		dvec3 df4 = p.coord[6] - taylor<11>(-p.dt, &force[4]);
+		dvec3 df5 = p.coord[7] - taylor<10>(-p.dt, &force[5]);
+		dvec3 df6 = p.coord[8] - taylor< 9>(-p.dt, &force[6]);
+		dvec3 df7 = p.coord[9] - taylor< 8>(-p.dt, &force[7]);
+		printf("[0]: %e %e %e\n", df0.x, df0.y, df0.z);
+		printf("[1]: %e %e %e\n", df1.x, df1.y, df1.z);
+		printf("[2]: %e %e %e\n", df2.x, df2.y, df2.z);
+		printf("[3]: %e %e %e\n", df3.x, df3.y, df3.z);
+		printf("[4]: %e %e %e\n", df4.x, df4.y, df4.z);
+		printf("[5]: %e %e %e\n", df5.x, df5.y, df5.z);
+		printf("[6]: %e %e %e\n", df6.x, df6.y, df6.z);
+		printf("[7]: %e %e %e\n", df7.x, df7.y, df7.z);
+	}
+};
+
